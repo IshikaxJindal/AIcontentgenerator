39ca5d043d44c19ad7fcf199efdb6347efa92d92
Ishika Jindal <ishika.jindal@gmail.com>
2025-02-12 22:47:35 +0530

    done

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div>
+      <h2>hiii</h2>
+      <Button>subscribe</Button>
+    </div>
+);}
